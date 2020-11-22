d20702db35a4e7d655c33c53394bb9294a9c70c3
Biplow Ghimire <biplow.ghimire@gmail.com>
2020-11-22 14:23:50 -0800

    sixth commit

--- lib/src/screens/cart.dart
+++ lib/src/screens/cart.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant_app/src/models/products.dart';
 import 'package:restaurant_app/src/widgets/custom_text.dart';
+
+import 'package:restaurant_app/src/screens/details.dart';
+import 'package:restaurant_app/src/widgets/featured_products.dart';
 
 class ShoppingCart extends StatefulWidget {
   @override
@@ -7,6 +11,15 @@ class ShoppingCart extends StatefulWidget {
 }
 
 class _ShoppingCartState extends State<ShoppingCart> {
+
+  Product product =  Product(
+      name: "Pork Momo",
+      price: 14.99,
+      rating: 4.2,
+      vendor: "GoodFos",
+      wishList: true,
+      image: "momo.png");
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,17 +66,45 @@ class _ShoppingCartState extends State<ShoppingCart> {
       backgroundColor: Colors.white,
       body: ListView(
         children: <Widget>[
-          Container(
-            height: 100,
-            width: MediaQuery.of(context).size.width - 10,
-            decoration: BoxDecoration(
-              color: Colors.red,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.red,
-                  offset: Offset(3, 2),
-                )
-              ]
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              height: 120,
+              width: MediaQuery.of(context).size.width - 10,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.red[500],
+                    offset: Offset(3, 5),
+                    blurRadius: 30,
+                  )
+                ]
+              ),
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset(
+                      "images/${product.image}",
+                      height: 100,
+                      width: 100,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      RichText(text: TextSpan(children: [
+                        TextSpan(text: product.name + "\n", style: TextStyle(color: Colors.black, fontSize: 20)),
+                        TextSpan(text: "\$" + product.price.toString(), style: TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold)),
+                      ]
+                      ),),
+
+                      IconButton(icon: Icon(Icons.delete, color: Colors.red,), onPressed: null,)
+                    ],
+                  )
+                ],
+              ),
             ),
           )
         ],
